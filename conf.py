b1221d3dfef1a9efb83d0784e44aeccd8fe7d492
Vik Qian <vik.qian@juphoon.com>
2019-08-28 14:39:55 +0800

    set index as master_doc

--- conf.py
+++ conf.py
@@ -23,7 +23,6 @@ author = 'Vik'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
-
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
